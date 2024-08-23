b37c262baa0dffc73daa8e7c915c099bd3025b3d
SkilledJohn <167893145+SkilledJohn@users.noreply.github.com>
2024-08-23 12:14:57 -0300

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,24 @@
+package Atividades;
+
+import javax.swing.*;
+
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+
+        int number = 0;
+        int result = 0;
+        StringBuilder tableBuilder = new StringBuilder();
+
+        number = Integer.parseInt(JOptionPane.showInputDialog("Informe o número para obter a tabuada da multiplicação: "));
+
+        tableBuilder.append("TABUADA DO ").append(number).append("\n\n");
+
+        for (int multiplier = 1; multiplier <= 10; multiplier++) {
+            result = multiplier * number;
+            tableBuilder.append(multiplier).append(" X ").append(number).append(" = ").append(result).append("\n");
+        }
+
+        JOptionPane.showMessageDialog(null, tableBuilder.toString());
+    }
+}
