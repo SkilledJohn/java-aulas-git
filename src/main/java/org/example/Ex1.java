503338b0fe10d0fedad8c77bf2081255f55401c5
unknown <js4867357@gmail.com>
2024-08-30 09:16:55 -0300

    commit dos erros corrigidos

---
+++ src/main/java/org/example/Ex1.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.swing.*;
+
+public class SumCalculator {
+
+    public static void main(String[] args) {
+
+        double number = 0;
+        double totalSum = 0;
+
+        for (int count = 0; count < 5; count++) {
+
+            number = Double.parseDouble(JOptionPane.showInputDialog("Digite um número para somar:"));
+
+            totalSum += number;
+        }
+
+        JOptionPane.showMessageDialog(null, "A soma total é: " + totalSum);
+    }
+}
